a255ea745c2c93db3fa9ebfa3e1859b836e40a0a
phsantosit <rivieiropedro@gmail.com>
2020-02-13 15:19:09 -0300

    Added py file to scrap amazon page

---
+++ scrap.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+driver = webdriver.Chrome()
+driver.get("https://amazon.com.br")
+
+inputElement = driver.find_element_by_id("twotabsearchtextbox")
+inputElement.send_keys("iphone")
+inputElement.submit()
+
+html = urlopen(driver.current_url)
+
+span = BeautifulSoup(html.read(), "html5lib")
+
+lista = []
+
+divs = span.findAll("div", {"class":"a-section a-spacing-medium"})
+
+for span in divs:
+    nametag = span.find("span", {"class":"a-size-base-plus"})
+    pricetag = span.find("span", {"class":"a-offscreen"})
+
+    if nametag and pricetag:
+            lista.append({
+                'Product':nametag.getText(),
+                'Price':pricetag.getText()
+                })
+
+print(lista)
